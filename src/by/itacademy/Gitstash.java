e6b2c77f9615dda85c3daaa8056882a918d2ad51
Nikroo <curiosityopportunity@gmail.com>
2021-09-20 17:39:03 +0300

    Added Hello Haven

--- src/by/itacademy/Gitstash.java
+++ src/by/itacademy/Gitstash.java
@@ -4,5 +4,6 @@ public class Gitstash {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Hello Inferno");
+        System.out.println("Hello Haven");
     }
 }
